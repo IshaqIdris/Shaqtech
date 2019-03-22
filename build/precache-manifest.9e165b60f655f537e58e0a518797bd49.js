9e4b8384b4507506fcab2accc5c232366b498ca1
Mohamed Idris <idrismoha2@ecs.vuw.ac.nz>
2019-03-23 10:55:34 +1300

    initial heroku commit

---
+++ build/precache-manifest.9e165b60f655f537e58e0a518797bd49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5553e32ab8e72c0b5b2e",
+    "url": "/static/js/main.6e2139fb.chunk.js"
+  },
+  {
+    "revision": "e5386c6a3a90c52fe241",
+    "url": "/static/js/2.60d69e59.chunk.js"
+  },
+  {
+    "revision": "5553e32ab8e72c0b5b2e",
+    "url": "/static/css/main.51b86e0d.chunk.css"
+  },
+  {
+    "revision": "f945e82384af6a9e70fc9800dcaf5f98",
+    "url": "/index.html"
+  }
+];
